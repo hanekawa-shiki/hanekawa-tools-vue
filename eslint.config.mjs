70c9afd7d2ab8983601f2377ae4d42389bd33cd3
shiki <kamiacgxu@gmail.com>
2026-09-08 22:26:59 +0800

    chore: update pnpm version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,47 +3,52 @@ import tailwindcss from 'eslint-plugin-tailwindcss';
 
 const tailwindcssRecommended = tailwindcss.configs.recommended;
 
-export default antfu({
-  stylistic: false,
-  vue: true,
-  formatters: {
-    css: 'prettier',
-    html: 'prettier',
-    markdown: 'prettier',
-  },
-  typescript: {
-    tsconfigPath: 'tsconfig.json',
-  },
-  settings: {
-    tailwindcss: {
-      cssConfigPath: 'src/index.css',
-      functions: ['clsx', 'classnames', 'cx', 'cn', 'cva'],
+export default antfu(
+  {
+    stylistic: false,
+    vue: true,
+    formatters: {
+      css: 'prettier',
+      html: 'prettier',
+      markdown: 'prettier',
+    },
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
+    },
+    settings: {
+      tailwindcss: {
+        cssConfigPath: 'src/index.css',
+        functions: ['clsx', 'classnames', 'cx', 'cn', 'cva'],
+      },
+    },
+    plugins: {
+      ...tailwindcssRecommended.plugins,
+    },
+    rules: {
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/enforces-negative-arbitrary-values': 'warn',
+      'tailwindcss/enforces-shorthand': 'warn',
+      'tailwindcss/no-arbitrary-value': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+      'tailwindcss/no-contradicting-classname': 'error',
+      'tailwindcss/no-unnecessary-arbitrary-value': 'warn',
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always',
+            normal: 'never',
+            component: 'always',
+          },
+        },
+      ],
+      'vue/singleline-html-element-content-newline': 'off',
+      // 强制所有 if/else/for/while 使用大括号
+      'antfu/curly': 'off',
+      curly: ['error', 'all'],
     },
   },
-  plugins: {
-    ...tailwindcssRecommended.plugins,
-  },
-  rules: {
-    'tailwindcss/classnames-order': 'warn',
-    'tailwindcss/enforces-negative-arbitrary-values': 'warn',
-    'tailwindcss/enforces-shorthand': 'warn',
-    'tailwindcss/no-arbitrary-value': 'off',
-    'tailwindcss/no-custom-classname': 'off',
-    'tailwindcss/no-contradicting-classname': 'error',
-    'tailwindcss/no-unnecessary-arbitrary-value': 'warn',
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'always',
-          normal: 'never',
-          component: 'always',
-        },
-      },
-    ],
-    'vue/singleline-html-element-content-newline': 'off',
-    // 强制所有 if/else/for/while 使用大括号
-    'antfu/curly': 'off',
-    curly: ['error', 'all'],
-  },
-});
+  {
+    ignores: ['pnpm-workspace.yaml'],
+  }
+);
